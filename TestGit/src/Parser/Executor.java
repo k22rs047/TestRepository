2d6b2d427bb86a9bfb3ca2b18bf36252df4820bd
User <User@LAPTOP-3SBGE5GS>
2026-08-24 23:19:52 +0900

    Helloを出力するメソッドを作成。

--- TestGit/src/Parser/Executor.java
+++ TestGit/src/Parser/Executor.java
@@ -3,6 +3,13 @@ package Parser;
 public class Executor {
 	
 	public static void main(String... args) {
+		for(int i = 0; i < 10; i++) {
+			printHello();
+		}
+	}
+	
+	
+	public static void printHello() {
 		System.out.println("Hello");
 	}
 
